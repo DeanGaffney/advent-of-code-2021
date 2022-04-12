f9c17f03ce6ce913ebfab779f2ba7f3b03d95b4a
Dean Gaffney <deanpatrickgaffney@gmail.com>
2022-04-12 22:00:36 +0100

    completed day 3 part 1

---
+++ day3-part-1/day3-part1.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+type IGammaRate interface {
+	addOne(int)
+	addZero(int)
+	getMostCommonBit() string
+}
+
+type GammaRate struct {
+	ones  int
+	zeros int
+}
+
+func (gammaRate *GammaRate) addOne(one int) {
+	gammaRate.ones++
+}
+
+func (gammaRate *GammaRate) addZero(zero int) {
+	gammaRate.zeros++
+}
+
+func (gammaRate GammaRate) getMostCommonBit() string {
+	if gammaRate.ones > gammaRate.zeros {
+		return "1"
+	} else {
+		return "0"
+	}
+}
+
+func (gammaRate GammaRate) getLeastCommonBit() string {
+	if gammaRate.ones > gammaRate.zeros {
+		return "0"
+	} else {
+		return "1"
+	}
+}
+
+func readBinaryNumbersFromFile() [][]string {
+	file, err := os.Open("../data/day3.txt")
+
+	if err != nil {
+		log.Fatal("Failed to open file")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var binaryNumbersMatrix [][]string
+
+	for scanner.Scan() {
+		binaryNumber := scanner.Text()
+
+		var row []string
+
+		for i := range binaryNumber {
+			character := string(binaryNumber[i])
+			row = append(row, character)
+		}
+
+		binaryNumbersMatrix = append(binaryNumbersMatrix, row)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return binaryNumbersMatrix
+}
+
+func main() {
+
+	var binaryNumbers [][]string = readBinaryNumbersFromFile()
+	var gammaRates []GammaRate
+
+	var numberOfRows = len(binaryNumbers)
+	var numberOfCols = len(binaryNumbers[0])
+
+	for i := 0; i < numberOfCols; i++ {
+
+		gammaRate := GammaRate{}
+
+		for j := 0; j < numberOfRows; j++ {
+			var character string = binaryNumbers[j][i]
+			if character == "1" {
+				gammaRate.addOne(1)
+			} else {
+				gammaRate.addZero(1)
+			}
+		}
+
+		gammaRates = append(gammaRates, gammaRate)
+	}
+
+	var mostCommonBits string
+	var leastCommonBits string
+
+	for i := range gammaRates {
+		gammaRate := gammaRates[i]
+		mostCommonBits += gammaRate.getMostCommonBit()
+		leastCommonBits += gammaRate.getLeastCommonBit()
+	}
+
+	rate, gammaRateErr := strconv.ParseInt(mostCommonBits, 2, 64)
+	epsilon, epsilonErr := strconv.ParseInt(leastCommonBits, 2, 64)
+
+	if gammaRateErr != nil || epsilonErr != nil {
+		log.Fatal("Failed to convert string to number")
+	}
+
+	fmt.Println(rate * epsilon)
+}
